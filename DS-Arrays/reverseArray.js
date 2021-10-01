e814b0abc54ed51be183f2c598bab0b1adb57bc5
Hassaan Zuberi <mhazuberi@gmail.com>
2021-09-30 20:44:56 -0600

    reverse Array advanced implementation complete

--- DS-Arrays/reverseArray.js
+++ DS-Arrays/reverseArray.js
@@ -1,13 +1,23 @@
 //Solution 0(n)
 const reverseString = (str) => {
+  //Check Input
+  if (!str || str.length < 2) return console.log("No String Found");
+
   const reversed = [];
   for (let i = str.length - 1; i >= 0; i--) {
-      reversed.push(str[i])
+    reversed.push(str[i]);
   }
   return reversed.join("");
-
 };
 
 const reversedString = reverseString("Hi My name is Hassaan");
 
 console.log(reversedString);
+
+//Solution Smarter ES6 way of reversing a String
+const reverseAString = (str) =>
+  !str || str.length < 2
+    ? "No String Found"
+    : [...str].reverse().join("");
+
+console.log(reverseAString("Hi My name is Hassaan"));
