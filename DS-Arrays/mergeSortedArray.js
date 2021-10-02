12452521415183b771178e13b99679cfd9f7482f
Hassaan Zuberi <mhazuberi@gmail.com>
2021-10-01 21:40:16 -0600

    merge sorted complete

--- DS-Arrays/mergeSortedArray.js
+++ DS-Arrays/mergeSortedArray.js
@@ -1,17 +1,34 @@
 console.log("MergeSorted");
 
 const mergeSorted = (arr1, arr2) => {
-  const finalArray = [];
-  for (let i = 0; i < arr1.length || i < arr2.length; i++) {
-    if (arr1[i] && arr2[i]) {
+  //Check Input
+  if (!arr1) return arr2;
+  if (!arr2) return arr1;
 
-    } else if (arr1[i]) {
-      console.log(arr1[i]);
+  const finalArray = [];
+
+  //To keep track which values are added.
+  let j = 0;
+  let k = 0;
+  for (let i = 0; i < arr1.length + arr2.length; i++) {
+    if (arr1[j] && arr2[k]) {
+      if (arr1[j] < arr2[k]) {
+        finalArray.push(arr1[j]);
+        //once added increment or point to the next value in array.
+        j++;
+      } else {
+        finalArray.push(arr2[k]);
+        k++;
+      }
+    } else if (arr1[j]) {
+      finalArray.push(arr1[j]);
+      j++;
     } else {
-      console.log(arr2[i]);
+      finalArray.push(arr2[k]);
+      k++;
     }
   }
   return finalArray;
 };
 
-mergeSorted([4, 6, 7, 10], [1, 3, 8, 10, 19, 21]);
+console.log(mergeSorted([4, 6, 7, 10], [1, 3, 8, 10, 19, 21]));
