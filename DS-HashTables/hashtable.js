a15e287b67f945d17241b4a3c035457e9afcc2d9
Hassaan Zuberi <mhazuberi@gmail.com>
2021-10-04 21:54:17 -0600

    hashtable key iterator with collison prevention.

--- DS-HashTables/hashtable.js
+++ DS-HashTables/hashtable.js
@@ -12,6 +12,7 @@ class HashTable {
   }
 
   set(key, value) {
+    //O(1)
     const address = this._hash(key);
     if (!this.data[address]) {
       this.data[address] = [];
@@ -21,6 +22,7 @@ class HashTable {
   }
 
   get(key) {
+    //O(1)
     const address = this._hash(key);
     if (!this.data[address]) {
       return;
@@ -32,19 +34,39 @@ class HashTable {
     }
   }
 
+  keys() {
+    //O(n)
+    const keyArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i] && this.data[i].length) {
+        if (this.data[i].length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            keyArray.push(this.data[i][j][0]);
+          }
+        } else {
+          keyArray.push(this.data[i][0][0]);
+        }
+      }
+    }
+    return keyArray;
+  }
+
   print() {
     console.log(this.data);
   }
 }
 
-const myHashTable = new HashTable(40);
+const myHashTable = new HashTable(49);
 
 myHashTable.print();
 
 myHashTable.set("grapes", 10000);
 myHashTable.set("pineapple", 40000);
 myHashTable.set("apples", 1000);
+myHashTable.set("bananas", 100);
 console.log(myHashTable.get("pineapple"));
 console.log(myHashTable.get("grapes"));
 
 myHashTable.print();
+
+console.log(myHashTable.keys());
