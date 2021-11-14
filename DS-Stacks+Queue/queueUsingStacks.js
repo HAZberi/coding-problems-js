d4bca0b50afa9698454b80826eeeb1a7a423ba3f
Hassaan Zuberi <mhazuberi@gmail.com>
2021-11-14 19:22:58 +0300

    implemented que using arrays or stacks

---
+++ DS-Stacks+Queue/queueUsingStacks.js
@@ -0,0 +1,39 @@
+class Queue {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+  enqueue(value) {
+    const length = this.first.length;
+    for (let i = 0; i < length; i++) {
+      this.last.push(this.first.pop());
+    }
+    this.last.push(value);
+    return this;
+  }
+  dequeue() {
+    const length = this.last.length;
+    for (let i = 0; i < length; i++) {
+      this.first.push(this.last.pop());
+    }
+    this.first.pop();
+    return this;
+  }
+  peek() {
+    if (this.first.length === 0) {
+      return this.last[0];
+    }
+    return this.first[this.first.length - 1];
+  }
+}
+
+const myQueue = new Queue();
+console.log(myQueue.enqueue("Hassaan"));
+console.log(myQueue.enqueue("Aamir"));
+console.log(myQueue.dequeue());
+console.log(myQueue.peek());
+console.log(myQueue.enqueue("Osman"));
+console.log(myQueue.dequeue());
+console.log(myQueue.peek());
+console.log(myQueue.enqueue("Butt"));
+console.log(myQueue.peek());
