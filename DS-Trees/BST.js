26f7c78aff4f5b8a56541a489003fde9a7fd4527
Hassaan Zuberi <mhazuberi@gmail.com>
2021-11-17 19:47:07 +0300

    inserting a value in BST

--- DS-Trees/BST.js
+++ DS-Trees/BST.js
@@ -10,10 +10,43 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
-  insert(value) {}
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+            if(!currentNode.left) {
+                currentNode.left = newNode;
+                break;
+            }
+            currentNode = currentNode.left;
+        } else {
+            if(!currentNode.right) {
+                currentNode.right = newNode;
+                break;
+            }
+            currentNode = currentNode.right;
+        }
+      }
+    }
+    return JSON.stringify(this.root);
+  }
   lookup(value) {}
   remove() {}
 }
+
+const myBST = new BinarySearchTree();
+console.log(myBST.insert(9));
+console.log(myBST.insert(4));
+console.log(myBST.insert(6));
+console.log(myBST.insert(1));
+console.log(myBST.insert(20));
+console.log(myBST.insert(15));
+console.log(myBST.insert(170));
+
 
 const traverse = (node) => {
   const tree = { value: node.value };
