c9708f48c74dfc57f96892872cdbc69a0fc454ec
Hassaan Zuberi <mhazuberi@gmail.com>
2021-11-22 19:04:18 +0300

    partial implementaion of remove node in BST

--- DS-Trees/BST.js
+++ DS-Trees/BST.js
@@ -51,7 +51,49 @@ class BinarySearchTree {
     }
     return false;
   }
-  remove() {}
+  remove(value) {
+    if (this.root === null) return false;
+    let currentNode = this.root;
+    let parentNode = null; //Tip: to track parent node initilize it as null;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (value === currentNode.value) {
+        if (currentNode.right === null) {
+          if (parentNode === null) {
+            this.root = currentNode.left;
+          } else {
+            if (currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            } else if (currentNode.value > parentNode.value) {
+              parentNode.right = currentNode.left;
+            }
+          }
+        } else if (currentNode.right.left === null) {
+          if(parentNode === null){
+            this.root = currentNode.left;
+          } else {
+            currentNode.right.left = currentNode.left;
+            if(currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.right;
+            } else if (currentNode.value > parentNode.value){
+              parentNode.right = currentNode.right;
+            }
+          }
+        } else {
+          if(parentNode === null){
+            this.root = currentNode.left;
+          } else {
+            
+          }
+        }
+      } else return false;
+    }
+  }
 }
 
 const myBST = new BinarySearchTree();
@@ -74,9 +116,9 @@ console.log(myBST.lookup(90));
 const traverse = (node) => {
   //start with a simple node without pointers
   const tree = { value: node.value };
-  //set the pointer to left node 
+  //set the pointer to left node
   tree.left = node.left !== null ? traverse(node.left) : null;
-  //set the pointer to right node 
+  //set the pointer to right node
   tree.right = node.right !== null ? traverse(node.right) : null;
   //reutrn the node once left and right pointers are found/returned
   return tree;
