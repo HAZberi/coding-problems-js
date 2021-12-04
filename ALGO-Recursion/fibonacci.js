418f9a29ae61c077452429291879242d72b53623
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-05 00:10:22 +0300

    fibonacci sequence complete

--- ALGO-Recursion/fibonacci.js
+++ ALGO-Recursion/fibonacci.js
@@ -1,9 +1,11 @@
 //Fibonacci Sequence Challenge
 //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 .......
 
+//BigO for the iterative approach is O(n)
 function fibonacciIterative(index) {
+  if (index <= 1) return index;
+
   const fibonacciSequence = [0, 1];
-  if (index < 2) return `Cannot create fibonacci sequence with ${index}`;
 
   for (let i = 1; i <= index; i++) {
     fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i]);
@@ -14,4 +16,15 @@ function fibonacciIterative(index) {
 
 console.log(fibonacciIterative(11));
 
+//BigO of the recursive approach is O(2^n) ===> Exponential
+//Test the functions using a value between 41-44 to see the difference
 
+function fibonacciRecursive(index) {
+  //Base Case
+  if (index <= 1) return index;
+
+  //Recursive Case
+  return fibonacciRecursive(index - 1) + fibonacciRecursive(index - 2);
+}
+
+console.log(fibonacciRecursive(4));
