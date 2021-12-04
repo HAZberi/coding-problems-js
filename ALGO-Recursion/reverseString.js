14d833b3b12a2ca88bff26aa6e8fe377c14ac471
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-05 01:50:50 +0300

    reverse a string recursively final

--- ALGO-Recursion/reverseString.js
+++ ALGO-Recursion/reverseString.js
@@ -1,10 +1,22 @@
 const reverseStringIterative = (string) => {
   const stringArray = [];
-  for (let i = string.length-1; i >= 0; i--) {
+  for (let i = string.length - 1; i >= 0; i--) {
     stringArray.push(string[i]);
   }
   return stringArray.join("");
 };
 
-console.log(reverseStringIterative("Hassaan"));
+console.log(reverseStringIterative("Yoyo Honey Singh's civic"));
 
+const reverseStringRecursive = (string) => {
+
+  //Base Case  
+  if (string.length === 1) return string[0];
+
+  //Recursive Case
+  const char = string[string.length - 1];
+  string = string.slice(0, -1);
+  return  `${char}${reverseStringRecursive(string)}`;
+};
+
+console.log(reverseStringRecursive("Badshah's civic lambo"));
