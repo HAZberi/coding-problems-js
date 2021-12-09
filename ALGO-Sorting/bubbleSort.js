db1ce598706dae5894cb01964b55ea195f0a4abf
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-09 20:08:01 +0300

    bubbleSort algo implemented

---
+++ ALGO-Sorting/bubbleSort.js
@@ -0,0 +1,26 @@
+const array = [99, 44, 23, 45, 646, 67, 98, 34];
+// const array2 = [34, 23, 44, 45, 67, 98, 99, 646];
+
+const bubbleSort = (array) => {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    console.log(`Entered Outer Loop with current value: ${array[i]}`);
+    for (let j = i + 1; j < length; j++) {
+      console.log(
+        `Entered inner Loop with current outer value: ${array[i]} and inner value ${array[j]}`
+      );
+      if (array[i] > array[j]) {
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+      console.log(
+        `Exited inner Loop with current outer value: ${array[i]} and inner value ${array[j]}`
+      );
+    }
+    console.log(`Exited Outer Loop with current value: ${array[i]}`);
+  }
+  return array;
+};
+
+console.log(bubbleSort(array));
