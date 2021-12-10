57795ddfcff8e2c175661343a3180c0ac8be63a3
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-10 23:46:23 +0300

    sorting fails at repeating values !Important

--- ALGO-Sorting/selectionSort.js
+++ ALGO-Sorting/selectionSort.js
@@ -1,19 +1,22 @@
-const array = [99, 44, 23, 45, 646, 67, 98, 34];
+const array = [99, 44, 23, 45, 646, 67, 98, 34, 45, 45];
 
 const selectionSort = (array) => {
   const length = array.length;
-  let lowestVal = array[0];
+
   for (let i = 0; i < length; i++) {
+    let lowestVal = array[i];
+
     for (let j = i + 1; j < length; j++) {
       if (lowestVal > array[j]) {
         lowestVal = array[j];
       }
     }
+
     const temp = array[i];
     array[array.indexOf(lowestVal)] = temp;
     array[i] = lowestVal;
-    lowestVal = array[i + 1];
-}
+  }
+
   return array;
 };
 
