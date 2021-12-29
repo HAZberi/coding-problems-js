dd24fc9b91b91c3ac19c478749e37d98a09a62ed
Hassaan Zuberi <mhazuberi@gmail.com>
2021-12-29 14:27:28 +0300

    breadthFirstSearchRecursive implemented.

--- ALGO-Searching/breadthFirstSearch.js
+++ ALGO-Searching/breadthFirstSearch.js
@@ -1,25 +1,31 @@
 const { BinaryTree } = require("../DS-Trees/BST.js");
 
 const breadthFirstSearch = (node) => {
-    let currentNode = node;
-    let list = [];
-    let queue = []; //using a queue is important here to keep track of child nodes in order. 
-    queue.push(currentNode);
-    while(queue.length > 0) {
-        currentNode = queue.shift();
-        list.push(currentNode.value);
-        if(currentNode.left){
-            queue.push(currentNode.left);
-        }
-        if(currentNode.right){
-            queue.push(currentNode.right);
-        }
+  let currentNode = node;
+  let list = [];
+  let queue = []; //using a queue is important here to keep track of child nodes in order.
+  queue.push(currentNode);
+  while (queue.length > 0) {
+    currentNode = queue.shift();
+    list.push(currentNode.value);
+    if (currentNode.left) {
+      queue.push(currentNode.left);
     }
-    return list;
-} 
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+  }
+  return list;
+};
 
-
-
+const breadthFirstSearchRecursive = (queue, list = []) => {
+  if (queue.length < 1) return list;
+  const currentNode = queue.shift();
+  list.push(currentNode.value);
+  if (currentNode.left) queue.push(currentNode.left);
+  if (currentNode.right) queue.push(currentNode.right);
+  return breadthFirstSearchRecursive(queue, list);
+};
 
 //Insert test values in a Binary Tree
 
@@ -34,3 +40,4 @@ BinaryTree.insert(170);
 // console.log(BinaryTree.lookup(20));
 
 console.log(breadthFirstSearch(BinaryTree.root));
+console.log(breadthFirstSearchRecursive([BinaryTree.root])); //initialize Queue by setting the root at function call
