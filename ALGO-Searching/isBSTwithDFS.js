f1ab8d68e844b65863cb87f9a605d9b24d42b9e2
Hassaan Zuberi <mhazuberi@gmail.com>
2022-01-02 20:34:32 +0300

    changed function name for correct representation

--- ALGO-Searching/isBSTwithDFS.js
+++ ALGO-Searching/isBSTwithDFS.js
@@ -7,7 +7,7 @@ class Node {
 }
 
 //Create A Random Tree with Node class
-const createTwoLevelBinaryTree = (arr) => {
+const createThreeLevelBinaryTree = (arr) => {
   const tree = new Node(arr[0] || null);
   tree.left = new Node(arr[1] || null);
   tree.right = new Node(arr[2] || null);
@@ -18,34 +18,33 @@ const createTwoLevelBinaryTree = (arr) => {
   return tree;
 };
 
-
 const isBST = (root) => {
-    const list = inorderTraversal(root);
-    return isSorted(list);
+  const list = inorderTraversal(root);
+  return isSorted(list);
 };
 
 const inorderTraversal = (node, list = []) => {
-    if (node === null) return list;
-    
-    if (node.left) {
-        inorderTraversal(node.left, list);
-    }
-    list.push(node.value);
-    if (node.right) {
-        inorderTraversal(node.right, list);
-    }
-    return list;
+  if (node === null) return list;
+
+  if (node.left) {
+    inorderTraversal(node.left, list);
+  }
+  list.push(node.value);
+  if (node.right) {
+    inorderTraversal(node.right, list);
+  }
+  return list;
 };
 
 const isSorted = (list) => {
-    console.log(list);
-    if (list.length < 2) return true;
-    for (let i = 0; i < list.length; i++) {
-        if (list[i-1] >= list[i]) return false;
-    }
-    return true;
+  console.log(list);
+  if (list.length < 2) return true;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i - 1] >= list[i]) return false;
+  }
+  return true;
 };
 
-const tree = createTwoLevelBinaryTree([9, 4, 20, 1, 6, 20, 30]);
+const tree = createThreeLevelBinaryTree([9, 4, 20, 1, 6, 20, 30]);
 
 console.log(isBST(tree));
