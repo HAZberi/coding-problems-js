d7e15a21960bb30b158d334b55eb349c80e7b815
Hassaan Zuberi <mhazuberi@gmail.com>
2022-01-02 20:50:56 +0300

    Valid BST w/ Inorder traversal refactor complete. Does Not work for equal node values

--- ALGO-Searching/isBSTwithDFS.js
+++ ALGO-Searching/isBSTwithDFS.js
@@ -1,24 +1,6 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
+const createThreeLevelBinaryTree = require('./utils/threeLevelBinaryTree'); 
 
-//Create A Random Tree with Node class
-const createThreeLevelBinaryTree = (arr) => {
-  const tree = new Node(arr[0] || null);
-  tree.left = new Node(arr[1] || null);
-  tree.right = new Node(arr[2] || null);
-  tree.left.left = new Node(arr[3] || null);
-  tree.left.right = new Node(arr[4] || null);
-  tree.right.left = new Node(arr[5] || null);
-  tree.right.right = new Node(arr[6] || null);
-  return tree;
-};
-
-const isBST = (root) => {
+const isBST = (root) => { //works only for lesser and greater values.
   const list = inorderTraversal(root);
   return isSorted(list);
 };
@@ -37,14 +19,13 @@ const inorderTraversal = (node, list = []) => {
 };
 
 const isSorted = (list) => {
-  console.log(list);
   if (list.length < 2) return true;
-  for (let i = 0; i < list.length; i++) {
+  for (let i = 1; i < list.length; i++) {
     if (list[i - 1] >= list[i]) return false;
   }
   return true;
 };
 
-const tree = createThreeLevelBinaryTree([9, 4, 20, 1, 6, 20, 30]);
+const tree = createThreeLevelBinaryTree([9, 4, 20, 2, 8, 19, 30]);
 
 console.log(isBST(tree));
