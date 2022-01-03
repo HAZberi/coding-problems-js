fa5c6fda7604636df353ce7cd2d04d1db318eae5
Hassaan Zuberi <mhazuberi@gmail.com>
2022-01-03 19:08:58 +0300

    find depth of the tree using bfs

--- ALGO-Searching/treeDepth.js
+++ ALGO-Searching/treeDepth.js
@@ -16,3 +16,19 @@ const tree = createThreeLevelBinaryTree([9, 4, 20, 1, 6, 15, 45]);
 console.log(tree);
 
 console.log(treeDepthWithRecursion(tree));
+
+//queue 
+// queue gets 1st node
+//current level number of nodes = 0
+//start outer loop 
+// current level number of nodes = size of queue
+// height + 1 
+//Start loop 
+// shift value from queue and set to current node
+// if left node exist -> add node to the queue 
+// if right node exist -> add node to the queue
+// current level nodes - 1; because we popped one node off when we remove the current node from the queue.
+
+//2nd approach 
+
+
