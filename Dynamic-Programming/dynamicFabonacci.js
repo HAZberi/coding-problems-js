f843fcc83be00a9ef7598d7c554d609075e5ad5c
Hassaan Zuberi <mhazuberi@gmail.com>
2022-01-23 22:36:13 +0300

    memoized implementation of fabonacci

--- Dynamic-Programming/dynamicFabonacci.js
+++ Dynamic-Programming/dynamicFabonacci.js
@@ -3,17 +3,39 @@
 // Basic Fabonacci implementation using cache while tracking number of calculation.
 
 let cache = {};
-let calculations = 0
+let calculations = 0;
 const fabonacci = (n) => {
-    if (cache[n]) return cache[n];
-    calculations++;
-    if(n <= 1) return n;
+  if (cache[n]) return cache[n];
+  calculations++;
+  if (n <= 1) {
+    cache[n] = n;
+    return cache[n];
+  }
+
+  cache[n] = fabonacci(n - 1) + fabonacci(n - 2);
+
+  return cache[n];
+};
+
+console.log(fabonacci(4));
+console.log(calculations);
+//console.log(cache);
+
+const memoizedFabonacci = () => {
+  let cache = {};
+  return function (n) {
+    if (n in cache) return cache[n];
+    if (n <= 1) {
+      cache[n] = n;
+      return cache[n];
+    }
 
     cache[n] = fabonacci(n - 1) + fabonacci(n - 2);
 
     return cache[n];
-}
+  };
+};
 
-console.log(fabonacci(10));
-console.log(calculations);
-//console.log(cache);
+const fabonacciNumber = memoizedFabonacci();
+
+console.log(fabonacciNumber(4));
