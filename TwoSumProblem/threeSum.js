8c37f84e825126fa2ab2d87e51ae7fd4324976e7
Hassaan Zuberi <mhazuberi@gmail.com>
2022-04-24 15:38:35 -0600

    threesum completed implementation

--- TwoSumProblem/threeSum.js
+++ TwoSumProblem/threeSum.js
@@ -8,22 +8,35 @@ const nums = [-1, 0, 1, 2, -1, -4]; //test case
 //Problem at hand: Remove Duplicates.
 
 const threeSum = (nums) => {
-  const dictionary = {};
-  const result = [];
+  let result = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[j] in dictionary) {
-        if (
-          i !== j &&
-          i !== dictionary[nums[j]] &&
-          j !== dictionary[nums[j]] &&
-          nums[i] + nums[j] + nums[dictionary[nums[j]]] === 0
-        ) {
-          result.push([nums[i], nums[j], nums[dictionary[nums[j]]]]);
-        }
+  //first sort the array
+  const sortedNums = nums.sort((a, b) => a - b);
+
+  //first pick an element
+  for (let i = 0; i < sortedNums.length; i++) {
+    //if element is a duplicate then skip
+    if (i > 0 && sortedNums[i] === sortedNums[i - 1]) continue;
+
+    //set left and right pointers for rest of the elements
+    let l = i + 1;
+    let r = sortedNums.length - 1;
+
+    while (l < r) {
+      //sum of fixed and left and right elements
+      const threeSum = sortedNums[i] + sortedNums[l] + sortedNums[r];
+
+      if (threeSum > 0) {
+        r--;
+      } else if (threeSum < 0) {
+        l++;
       } else {
-        dictionary[0 - nums[i] - nums[j]] = j;
+        result.push([sortedNums[i], sortedNums[l], sortedNums[r]]);
+
+        //for next iteration we need to update the pointers
+        //consider edge case of sortedNums input for l and r only [-2, -2, 0, 0, 2, 2]
+        l++;
+        while (sortedNums[l] === sortedNums[l - 1] && l < r) l++;
       }
     }
   }
