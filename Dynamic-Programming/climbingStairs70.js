32d47a66ae99dc8bdcc44bb701e8e29b9b94757a
Hassaan Zuberi <mhazuberi@gmail.com>
2022-05-19 15:08:24 -0600

    climbing stairs second solution

--- Dynamic-Programming/climbingStairs70.js
+++ Dynamic-Programming/climbingStairs70.js
@@ -16,3 +16,23 @@ const climbingStairs = (n) => {
 };
 
 console.log(climbingStairs(5));
+
+//2nd Approach Climbing Stair Problem Bottom up approach
+
+const climbingStairs2 = (n) => {
+  if(n === 0) return 0;
+
+  //Intialize stair with ways to climb step 0
+  const stair = [0];
+  //possible ways to climb one step -- sub problem
+  stair[1] = 1;
+  //possible ways to climb second step -- sub problem
+  stair[2] = 2;
+  //summing up subproblem to get the value for original problem.
+  for(let i = 3; i<=n; i++){
+    stair[i] = stair[i-1] + stair[i-2];
+  } 
+  return stair[n];
+}
+
+console.log(climbingStairs2(5));
